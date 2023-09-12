9301007c34bf71eb3c609711d9db611986a269c5
Mefodii <99871267+DonBal1on@users.noreply.github.com>
2023-09-12 18:29:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double number = 4.5;
+
+        // Округление до ближайшего целого числа
+        int roundToNearest = (int)Math.Round(number);
+        int ceiling = (int)Math.Ceiling(number);
+        int floor = (int)Math.Floor(number);
+
+        Console.WriteLine($"Значение аргумента: {number}");
+        Console.WriteLine($"Округление до ближайшего целого: {roundToNearest}");
+        Console.WriteLine($"Округление вверх: {ceiling}");
+        Console.WriteLine($"Округление вниз: {floor}");
+
+        number = -4.5;
+
+        roundToNearest = (int)Math.Round(number);
+        ceiling = (int)Math.Ceiling(number);
+        floor = (int)Math.Floor(number);
+
+        Console.WriteLine($"Значение аргумента: {number}");
+        Console.WriteLine($"Округление до ближайшего целого: {roundToNearest}");
+        Console.WriteLine($"Округление вверх: {ceiling}");
+        Console.WriteLine($"Округление вниз: {floor}");
+    }
+}
